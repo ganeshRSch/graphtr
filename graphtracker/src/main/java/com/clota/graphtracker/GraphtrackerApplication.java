d99ccf7c5bb00790f55a50019fd5034e9e4ec336
Ganesh <mca.ganesh@gmail.com>
2022-09-19 23:05:45 -0400

    Main Branch

---
+++ graphtracker/src/main/java/com/clota/graphtracker/GraphtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.clota.graphtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphtrackerApplication.class, args);
+	}
+
+}
---
+++ graphtracker/src/test/java/com/clota/graphtracker/GraphtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clota.graphtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
