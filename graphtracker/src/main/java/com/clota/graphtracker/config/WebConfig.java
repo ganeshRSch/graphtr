39bfb305747f9aea401079c0ab2253fd630f194c
Ganesh <mca.ganesh@gmail.com>
2022-09-24 13:24:37 -0400

    Added User Rest Controller

--- graphtracker/src/main/java/com/clota/graphtracker/config/WebConfig.java
+++ graphtracker/src/main/java/com/clota/graphtracker/config/WebConfig.java
@@ -1,17 +1,30 @@
 package com.clota.graphtracker.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+  private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+          "classpath:/META-INF/resources/", "classpath:/resources/",
+          "classpath:/static/", "classpath:/public/" };
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").allowedOrigins("*");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    if (!registry.hasMappingForPattern("/webjars/**")) {
+      registry.addResourceHandler("/webjars/**").addResourceLocations(
+              "classpath:/META-INF/resources/webjars/");
+    }
+    if (!registry.hasMappingForPattern("/**")) {
+      registry.addResourceHandler("/**").addResourceLocations(
+              CLASSPATH_RESOURCE_LOCATIONS);
+    }
   }
 } 
--- graphtracker/src/main/java/com/clota/graphtracker/restcontroller/RestUserController.java
+++ graphtracker/src/main/java/com/clota/graphtracker/restcontroller/RestUserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +14,12 @@ import java.util.List;
 public class RestUserController {
 
 
-    @GetMapping(path = "/user", produces = "application/json")
+   /* @GetMapping("/")
+    public String index() {
+        return "/index.html";
+    }*/
+
+    @GetMapping(path = "user", produces = "application/json")
     public List<User> user() {
         List<User> list = new ArrayList<>();
         User user = new User();
@@ -23,7 +29,7 @@ public class RestUserController {
         return list;
     }
 
-   @PostMapping(path = "/userInput", consumes = MediaType.APPLICATION_JSON_VALUE)
+   @PostMapping(path = "userInput", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void userInput(@RequestBody User user) {
 
         System.out.println("User :" + user.getEmail());
