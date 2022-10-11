b3b8740797ca2e59887a8062803f00e50d0a8bfb
Ganesh <mca.ganesh@gmail.com>
2022-10-11 08:19:23 +0530

    Department Entity

---
+++ graphtracker/src/main/java/com/clota/graphtracker/entity/Department.java
@@ -0,0 +1,65 @@
+package com.clota.graphtracker.entity;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name = "department")
+public class Department {
+
+	@Column
+	Integer id;
+	@Column
+	String name;
+	@Column
+	String description;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+
+}
